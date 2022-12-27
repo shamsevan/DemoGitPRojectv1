534268ec7e3c41903453f87f0ccd8367ffe70fc1
EVAN <EVAN@192.168.0.26>
2022-12-26 22:08:14 -0500

    Hey guys , this is the v1 project. you have to work in it

---
+++ src/Tasks_Package/test1.java
@@ -0,0 +1,20 @@
+package Tasks_Package;
+
+public class test1 {
+
+	public static void main(String[] args) {
+		String sleepy="yes";
+		
+		if(sleepy=="yes") {
+			System.out.println("have some sleep");
+		}
+		
+		else {
+			System.out.println("work.....");
+		}
+		
+		
+
+	}
+
+}
