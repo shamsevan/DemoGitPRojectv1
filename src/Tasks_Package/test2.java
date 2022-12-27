7632a60d08ab6dbe5336fc0c772f32cf0dbef8c4
EVAN <EVAN@192.168.0.26>
2022-12-26 22:40:11 -0500

    hi lead,

---
+++ src/Tasks_Package/test2.java
@@ -0,0 +1,18 @@
+package Tasks_Package;
+
+public class test2 {
+
+	public static void main(String[] args) {
+String sleepy="yes";
+		
+		if(sleepy=="yes"|| sleepy=="y") {
+			System.out.println("have some sleep");
+		}
+		
+		else {
+			System.out.println("work.....");
+		}
+
+	}
+
+}
